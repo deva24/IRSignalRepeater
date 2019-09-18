396465c2b2dc4456e8bd555dbcbb2c62583549ce
Devarshi Hazarika <49090172+deva24@users.noreply.github.com>
2019-09-18 22:46:10 +0530

    load and save implementated

--- PlayMemories_Home.lnk/PlayMemories_Home.lnk.ino
+++ PlayMemories_Home.lnk/PlayMemories_Home.lnk.ino
@@ -7,9 +7,11 @@
  *     88    `8b  d8' 88  .8D `8b  d8' 
  *     YP     `Y88P'  Y8888D'  `Y88P'  
  * 
+ *  ► Pending
+ * 
  *  x Implement playback
- *  ► Implement save
- *  ► Implement load
+ *  x Implement save
+ *  x Implement load
  * 
  */
 
@@ -29,6 +31,13 @@ const char *ssid = STASSID;
 const char *password = STAPSK;
 
 ESP8266WebServer server(80);
+#define send_ok server.send(200, "application/json", "{\"status\":\"ok\"}")
+void send_err(String x)
+{
+	Serial.print("Error : ");
+	Serial.println(x);
+	server.send(200, "application/json", "{\"status\":\"failed\",\"reason\":\"" + x + "\"}");
+}
 
 #pragma region pin config
 #define PIN_RX D1
@@ -49,7 +58,7 @@ void blink(unsigned long wait_on, unsigned long wait_off)
 
 #pragma endregion
 
-#pragma region Recorder config
+#pragma region Recorder service
 
 const int PIN_RXT = digitalPinToInterrupt(PIN_RX);
 #define RecordTimeout 200
@@ -88,39 +97,41 @@ void recorderLoop()
 void handle_record_service()
 {
 	blink(200, 500);
+	signalBuffer.clear();
 	attachInterrupt(PIN_RXT, rx_isr, CHANGE);
+
+	send_ok;
 }
 
 void endofRecord()
 {
 	blink(100, 500);
-	
-	unsigned long off=signalBuffer.get(0);
+
+	unsigned long off = signalBuffer.get(0);
 	unsigned long temp = 0;
 	unsigned long diff = 0;
-	LinkedList <unsigned long>tempBuff;
+	LinkedList<unsigned long> tempBuff;
 
 	// saving the delta in tempbuff
 
-	for (int i=1; i<signalBuffer.size(); i++)
+	for (int i = 1; i < signalBuffer.size(); i++)
 	{
 		temp = signalBuffer.get(i);
-	
-		if(temp>off)
-			diff = temp-off;
+
+		if (temp > off)
+			diff = temp - off;
 		else
-			diff=temp + (ULONG_MAX - off);
+			diff = temp + (ULONG_MAX - off);
 
 		tempBuff.add(diff);
-		off=temp;
+		off = temp;
 	}
-
 
 	//copying tempbuff to sigbuff;
 	signalBuffer.clear();
-	for(int i=0; i<tempBuff.size();i++)
+	for (int i = 0; i < tempBuff.size(); i++)
 		signalBuffer.add(tempBuff.get(i));
-	
+
 	tempBuff.clear();
 	blink(500, 500);
 }
@@ -134,10 +145,10 @@ void handle_fetch_service()
 	String ret = "{\"timecode\":[0";
 
 	int len = signalBuffer.size();
-	unsigned long acc=0;
+	unsigned long acc = 0;
 	for (int i = 0; i < len; i++)
 	{
-		acc+=signalBuffer.get(i);
+		acc += signalBuffer.get(i);
 		ret += "," + String(acc);
 	}
 
@@ -154,33 +165,170 @@ void handle_playback_service()
 	int len = signalBuffer.size();
 	bool state = true;
 
-	analogWriteFreq ( 40000 );
-	analogWrite ( PIN_CW, 511 );
+	analogWriteFreq(40000);
+	analogWrite(PIN_CW, 511);
 
-	blink(200,1000);
+	blink(200, 1000);
 
-	for(int i=0; i<len; i++)
+	for (int i = 0; i < len; i++)
 	{
-		if(state)
-			digitalWrite ( PIN_TX, HIGH);
+		if (state)
+			digitalWrite(PIN_TX, HIGH);
 		else
-			digitalWrite ( PIN_TX, LOW);
-		
+			digitalWrite(PIN_TX, LOW);
+
 		delayMicroseconds(signalBuffer.get(i));
 
 		state = !state;
 	}
-	digitalWrite( PIN_TX, LOW );
+	digitalWrite(PIN_TX, LOW);
 
 	delay(1000);
-	blink(200,1000);
+	blink(200, 1000);
+
+	send_ok;
+}
+#pragma endregion
+
+#pragma region handle save functionality
+void handle_save_request()
+{
+	String sigName = "";
+	for (int i = 0; i < server.args(); i++)
+	{
+		if (server.argName(i) == "name")
+		{
+			sigName = server.arg(i);
+			break;
+		}
+	}
+	if (sigName == "")
+	{
+		send_err("Query string parameter argument 'name' not found.");
+		return;
+	}
+
+	File f = SPIFFS.open("/sig/" + sigName + ".sig", "w");
+	f.write(0);
+
+	unsigned long temp;
+	char b1;
+	char b2;
+	char b3;
+
+	for (int i = 0; i < signalBuffer.size(); i++)
+	{
+		temp = signalBuffer.get(i);
+		b3 = 0xFF & temp;
+		temp >>= 8;
+		b2 = 0xFF & temp;
+		temp >>= 8;
+		b1 = 0xFF & temp;
+
+		f.write(b1);
+		f.write(b2);
+		f.write(b3);
+	}
+
+	f.close();
+
+	send_ok;
+}
+#pragma endregion
+
+#pragma region handle list signals
+void handle_signal_list()
+{
+	Dir dir1 = SPIFFS.openDir("/sig");
+	String ret = "{\"d\":[";
+	bool isFirst = true;
+	while (dir1.next())
+	{
+		if (dir1.isFile())
+		{
+			if (!isFirst)
+			{
+				ret += ",";
+			}
+			isFirst = false;
+			String fileName = dir1.fileName();
+			fileName = fileName.substring(5, fileName.length() - 4);
+
+			ret += "{\"name\":\"" + fileName + "\"}";
+		}
+	}
+	ret += "]}";
+
+	server.send(200, "application/json", ret);
+}
+#pragma endregion
+
+#pragma region load signal
+void handle_load_signal()
+{
+	String sigName = "";
+	bool doFail = true;
+
+	for (int i = 0; i < server.args(); i++)
+	{
+		if (server.argName(i) == "sig")
+		{
+			sigName = server.arg(i);
+			doFail = false;
+			break;
+		}
+	}
+
+	if (doFail)
+	{
+		send_err("Coudln't find query string parameter 'sig'");
+		return;
+	}
+
+	String aSigName = "/sig/" + sigName + ".sig";
+
+	if (SPIFFS.exists(aSigName))
+	{
+		File sig = SPIFFS.open(aSigName, "r");
+
+		char version = sig.read();
+		if (version != 0)
+		{
+			send_err("Signal file version " + String((byte)version) + " is not supported.");
+			sig.close();
+			return;
+		}
+
+		unsigned long delta;
+
+		signalBuffer.clear();
+		while (sig.available())
+		{
+			delta = sig.read();
+
+			delta <<= 8;
+			delta |= sig.read();
+
+			delta <<= 8;
+			delta |= sig.read();
+
+			signalBuffer.add(delta);
+		}
+
+		send_ok;
+		sig.close();
+	}
+	else
+	{
+		send_err("Signal '" + sigName + "' not found.");
+		return;
+	}
 }
 #pragma endregion
 
 #pragma region http server
 
-String
-determineContentType(String url)
+String determineContentType(String url)
 {
 	String ct;
 	String ext = "";
@@ -235,27 +383,14 @@ void handleNotFound()
 
 void setupServer()
 {
-	server.on("/inline", []() {
-		server.send(200, "text/plain", "this works as well");
-	});
-
-	// @todo
-	// implement services
-
 	server.on("/record.service", handle_record_service);
 	server.on("/fetchRecord.service", handle_fetch_service);
 	server.on("/playback.service", handle_playback_service);
-
-	server.on("/saveRecord.service", []() {
-
-	});
-
-	server.on("/listRecord.service", []() {
-
-	});
-
-	server.on("/loadsignal.service", []() {
-
+	server.on("/saveRecord.service", handle_save_request);
+	server.on("/listRecord.service", handle_signal_list);
+	server.on("/loadsignal.service",[](){
+		handle_load_signal();
+		handle_playback_service();
 	});
 
 	server.on("/trim.service", []() {
